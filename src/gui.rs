ea27a11e02797885d6d25a0e8c8bce1e5351c577
lapiks <dhenin.julien@gmail.com>
2024-02-07 22:15:00 +0100

    full screen egui render

--- src/gui.rs
+++ src/gui.rs
@@ -2,8 +2,11 @@ pub struct Gui {}
 
 impl Gui {    
     pub fn draw(&mut self, egui_ctx: &egui::Context, game_texture: egui::TextureId) {
-        egui::Window::new("Polytron").show(egui_ctx, |ui| {
-            ui.image(game_texture, egui::Vec2::new(1000.0, 1000.0));
-        });
+        egui::Window::new("MainWindow")
+            .constrain(true)
+            .title_bar(false)
+            .show(egui_ctx, |ui| {
+                ui.image(game_texture, egui_ctx.available_rect().size());
+            });
     }
 }
