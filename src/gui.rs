c4ee733839537678e4d4e9267e3d363d69e01cfc
lapiks <dhenin.julien@gmail.com>
2024-02-09 15:18:34 +0100

    remove egui_tiles

--- src/gui.rs
+++ src/gui.rs
@@ -3,8 +3,11 @@ pub struct Gui {}
 impl Gui {    
     pub fn draw(&mut self, egui_ctx: &egui::Context, game_texture: egui::TextureId) {
         egui::CentralPanel::default()
-            .show(egui_ctx, |ui| {
-                ui.image(game_texture, egui_ctx.available_rect().size());
+            .show(egui_ctx, |ui| {  
+                ui.image(
+                    game_texture, 
+                    egui_ctx.available_rect().size()
+                );
             });
     }
 }
