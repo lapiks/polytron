46307b0f46968f94db6aaca7cda8e0e470be2e59
lapiks <dhenin.julien@gmail.com>
2024-02-09 14:54:47 +0100

    show image in a central panel

--- src/gui.rs
+++ src/gui.rs
@@ -2,9 +2,7 @@ pub struct Gui {}
 
 impl Gui {    
     pub fn draw(&mut self, egui_ctx: &egui::Context, game_texture: egui::TextureId) {
-        egui::Window::new("MainWindow")
-            .constrain(true)
-            .title_bar(false)
+        egui::CentralPanel::default()
             .show(egui_ctx, |ui| {
                 ui.image(game_texture, egui_ctx.available_rect().size());
             });
